abc455ea6eca6cda46dd4921b9c828a8dfb3644e
Nitin Kushwaha <nk17oct@gmail.com>
2022-12-17 20:41:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,14 @@
 console.log("Service Worker Loaded...");
 
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
+
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'image',
+  new workbox.strategies.CacheFirst()
+);
+
 self.addEventListener("push", e => {
   const data = e.data.json();
   console.log("Push Recieved...");
