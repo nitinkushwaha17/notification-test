1c146f88718f4f3c126df72c964cc76181b310a0
Nitin Kushwaha <nk17oct@gmail.com>
2022-12-17 19:00:38 +0530

    Update client.js

--- client.js
+++ client.js
@@ -25,7 +25,7 @@ async function send() {
 
   // Send Push Notification
   console.log("Sending Push...");
-  await fetch("notification-server-production.up.railway.app/subscribe", {
+  await fetch("https://notification-server-production.up.railway.app/subscribe", {
     method: "POST",
     body: JSON.stringify(subscription),
     headers: {
